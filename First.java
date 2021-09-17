5826e74c91297258a83f55a58c12d33f85cc5be0
siddarthjha <siddarthkumarjha1999@gmail.com>
2021-09-17 12:37:56 +0530

    First Question

--- First.java
+++ First.java
@@ -1,6 +1,33 @@
 public class
 {
-public static void main(String[] args)
-{
-}
+    public static void main(String[] args)
+    {
+        int temp;  
+        //initializing an array  
+        int a[]={12,5,56,-2,32,2,-26,9,43,94,-78};  
+        for (int i = 0; i < a.length; i++)   
+        {  
+            for (int j = i + 1; j < a.length; j++)   
+            {  
+                if (a[i] < a[j])   
+                {  
+                    temp = a[i];  
+                    a[i] = a[j];  
+                    a[j] = temp;  
+                }  
+            }     
+        }  
+        System.out.println("Array elements in descending order:");  
+        //accessing element of the array  
+        for (int i = 0; i <=a.length - 1; i++)   
+        {  
+            System.out.println(a[i]);  
+        }
+        System.out.println("Array elements in ascending order:");  
+        //accessing element of the array  
+        for (int i = a.length - 1; i <=0; i--)   
+        {  
+            System.out.println(a[i]);  
+        }
+    }
 }
